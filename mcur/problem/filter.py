8f9a071882e72cb8a0ef6335361fdb4e73687f65
kalan4iki <kalan-4@ya.ru>
2020-02-19 16:33:39 +0300

    update

--- mcur/problem/filter.py
+++ mcur/problem/filter.py
@@ -8,7 +8,7 @@ class ProblemFilter(django_filters.FilterSet):
     #status = django_filters.CharFilter(lookup_expr='iexact')
     class Meta:
         model = Problem
-        fields = ['temat', 'status']
+        fields = ['temat', 'status', 'statussys']
 
 class ProblemListView(SingleTableMixin, FilterView):
     table_class = ProblemTable
