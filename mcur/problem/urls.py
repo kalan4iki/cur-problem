735a6131cf236ab2a618f2f274721d134066b6aa
kalan4iki <kalan-4@ya.ru>
2020-02-20 11:16:24 +0300

    Bugs

--- mcur/problem/urls.py
+++ mcur/problem/urls.py
@@ -30,8 +30,8 @@ urlpatterns = [
     url(r'prosrproblem/', ProblemProsrListView.as_view(), name='prosrproblem'),
     url(r'^export/xls/$', exportxls, name='exportxls'),
     path('term/<int:pk>', termview, name='termview'),
-    url(r'answer/action/aprrove/<int:pk>', AnswerAction.Approve, name='term_approve'),
-    url(r'answer/action/modify/<int:pk>', AnswerAction.Modify, name='term_modify'),
+    url(r'answer/action/aprrove/<int:pk>', AnswerAction.Approve(), name='term_approve'),
+    path('answer/action/modify/<int:pk>', AnswerAction.Modify(), name='term_modify'),
 ]
 #    path('allproblem/', allproblem, name='allproblem'),
 
