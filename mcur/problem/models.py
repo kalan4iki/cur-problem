c13ddee9687c87a67dd1324ad6ba9d1efcf7501e
kalan4iki <kalan-4@ya.ru>
2020-02-20 09:53:53 +0300

    Update

--- mcur/problem/models.py
+++ mcur/problem/models.py
@@ -196,11 +196,12 @@ class Answer(models.Model):
 
     def __str__(self):
         return f'{self.datecre.day}.{self.datecre.month}.{self.datecre.year}'
-    #def __str__(self):
+
     def get_absolute_url(self):
         return reverse() # TODO: Доработать
+
     def get_status_display(self):
-        return stats[self.status]
+        return self.status
 
 class Image(models.Model):
     otv = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, default=None, related_name='img', blank=True,
